729ab1756c769782f4e0961f41aa7b64e42c8535
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2017-09-24 15:18:52 +0200

    used format_lazy() instead of string_concat() on Django > 1.10 for Django 2.1 compatibility.

--- metrics/traffic.py
+++ metrics/traffic.py
@@ -23,10 +23,11 @@
 
 from time import mktime
 
+import django
 from django.core.exceptions import ImproperlyConfigured
 from django.db.models import Count
 from django.utils.translation import gettext_lazy as _
-from django.utils.translation import string_concat, gettext
+from django.utils.translation import gettext
 
 from . import settings
 from .utils import get_verbose_name
@@ -108,8 +109,14 @@ class Module:
 
         if not hasattr(self, "verbose_name"):
             self.verbose_name = get_verbose_name(self.module_name)
+
         if not hasattr(self, "verbose_name_plural"):
-            self.verbose_name_plural = string_concat(self.verbose_name, "s")
+            if django.VERSION < (1, 11):
+                from django.utils.translation import string_concat
+                self.verbose_name_plural = string_concat(self.verbose_name, 's')
+            else:
+                from django.utils.text import format_lazy
+                self.verbose_name_plural = format_lazy('{}{}', self.verbose_name, 's')
 
     def count(self, qs):
         raise NotImplementedError("'count' isn't defined.")
